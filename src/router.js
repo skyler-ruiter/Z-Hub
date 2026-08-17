5d0aceb1373bacfb21ecbe43b881afae9d583c6f
Guoxi <liuguoxi888@gmail.com>
2026-08-17 13:44:44 -0400

    Use hash-based routing to fix 404 on GitHub Pages refresh

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import Home from './pages/Home.vue';
 import Modules from './pages/Modules.vue';
@@ -15,7 +15,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
